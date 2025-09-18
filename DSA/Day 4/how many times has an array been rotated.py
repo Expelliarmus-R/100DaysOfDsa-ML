a74d3f4f9713eea25765f27f0e54ddb72f8fe2c8
Rakesh Kumar <140553520+Expelliarmus-R@users.noreply.github.com>
2025-09-18 23:47:00 +0530

    Add files via upload

---
+++ DSA/Day 4/how many times has an array been rotated.py
@@ -0,0 +1,29 @@
+def rotations(arr):
+    n=len(arr)
+    low=0
+    high=n-1
+    ans=float('inf')
+    index=-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[low]<=arr[high]:
+            if arr[low]<ans:
+                index=low
+                ans=arr[low]
+            break
+        if arr[low]<=arr[mid]:
+            if arr[low]<ans:
+                index=low
+                ans=arr[low]
+            low=mid+1
+        else:
+            if arr[mid]<ans:
+                index=mid
+                ans=arr[mid]
+            high=mid-1
+    return index
+arr=[3,4,5,1,2]
+s=rotations(arr)
+print(s)
+
+
